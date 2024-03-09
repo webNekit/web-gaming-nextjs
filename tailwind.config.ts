6de399a74331c9f0131e4aa8ad622be87aacc08f
webNekit <mochalov-hunter@mail.ru>
2024-03-09 11:28:13 +0300

    Настроили конфиг tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,28 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '15px'
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      oswald: 'var(--font-oswald)',
+      roboto: 'var(--font-roboto)',
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        accent: '#d4000d'
       },
     },
   },
