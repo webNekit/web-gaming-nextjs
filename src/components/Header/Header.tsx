b8be3faae82c4dfe87d0a6922647b82a40e62656
webNekit <mochalov-hunter@mail.ru>
2024-03-09 12:15:10 +0300

    Добавили логотип и компонент Navbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import Navbar from '../Navbar/Navbar';
 
 const Header = () => {
   return (
-    <header>
-      header 
+    <header className='w-full bg-zinc-950'>
+      <div className="container mx-auto">
+        <div className="w-full flex items-center justify-between">
+          <Link href="/" className='w-[200px] h-[50px]'>
+            <Image src='/assets/img/logo.svg' alt='Логотип сайта' width={200} height={50} className='w-full h-full' />
+          </Link>
+          <Navbar containerStyle='' />
+        </div>
+      </div> 
     </header>
   )
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,29 @@
+import Link from 'next/link';
 import React from 'react';
+
+// menuData 
+const menuLinks = [
+  { label: 'Главная', url: '/' },
+  { label: 'О нас', url: '/about' },
+  { label: 'Акции', url: '/sales' },
+  { label: 'Каталог', url: '/catalog' },
+  { label: 'Контакты', url: '/contacts' }
+];
 
 const Navbar = ( { containerStyle } : { containerStyle: string } ) => {
   return (
     <nav className={`${containerStyle}`}>
-      <menu className="w-full flex items-center justify-center gap-4"></menu>
+      <menu className="w-full flex items-center justify-center gap-4">
+        {menuLinks.map(( item, index ) => {
+          return (
+            <>
+              <li key={index}>
+                <Link href={item.url}>{item.label}</Link>
+              </li>
+            </>
+          )
+        })}
+      </menu>
     </nav>
   )
 }
