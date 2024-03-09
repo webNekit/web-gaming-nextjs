f7d91b5370996a02715fc4345a8ed577648211bd
webNekit <mochalov-hunter@mail.ru>
2024-03-09 12:37:19 +0300

    Начали верстку мобильного меню

--- src/components/NavbarMobile/NavbarMobile.tsx
+++ src/components/NavbarMobile/NavbarMobile.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 
-const NavbarMobile = () => {
+const menuLinks = [
+  { label: 'Главная', url: '/' },
+  { label: 'О нас', url: '/about' },
+  { label: 'Акции', url: '/sales' },
+  { label: 'Каталог', url: '/catalog' },
+  { label: 'Контакты', url: '/contacts' }
+];
+
+const NavbarMobile = ({ containerStyle } : { containerStyle: string }) => {
   return (
-    <nav>
-      Мобильное меню
+    <nav className={`${containerStyle}`}>
+      <menu className="">
+        {menuLinks.map((item, index) => {
+          return (
+            <>
+              <li key={index}>
+                <Link href={item.url}>{item.label}</Link>
+              </li>
+            </>
+          );
+        })}
+      </menu>
     </nav>
   )
 }
