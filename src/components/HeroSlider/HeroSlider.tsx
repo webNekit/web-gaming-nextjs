7e49047c6fca7e45aaeac37f6dfde7fa4bc79bcf
webNekit <mochalov-hunter@mail.ru>
2024-03-12 18:49:31 +0300

    Добавили проверку на длину символов

--- src/components/HeroSlider/HeroSlider.tsx
+++ src/components/HeroSlider/HeroSlider.tsx
@@ -16,7 +16,7 @@ const HeroSlider = () => {
     },
     {
       title: 'Lorem ipsum dolor sit amet.',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident quisquam repellat incidunt? Facere ad, animi quas recusandae fuga commodi iusto officiis consequatur, sed quisquam atque?',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. sit amet, consectetur adipisicing elit. Provident quisquam repellat incidunt? Facere ad, animi quas recusandae fuga commodi iusto officiis consequatur, sed quisquam atque?',
     },
     {
       title: 'Lorem ipsum dolor sit amet.',
@@ -29,9 +29,9 @@ const HeroSlider = () => {
       {SlideData.map((item, index) => {
         return (
           <SwiperSlide key={index} className=''>
-            <div className="w-full">
-              <h2 className="">{item.title}</h2>
-              <p className="">{item.description}</p>
+            <div className="w-full md:w-1/2">
+              <h2 className="mb-8 text-8xl">{item.title}</h2>
+              <p className="text-xl mb-12">{`${item.description.length > 189 ? item.description.slice(0, 189) + '...' : item.description }`}</p>
               <CustomButton containerStyle={'w-[170px] h-[50px]'} text='Подробнее' />
             </div>
           </SwiperSlide>
