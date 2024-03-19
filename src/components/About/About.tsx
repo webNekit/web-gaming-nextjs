f182779c56f02d3c9fa951f47eade01d9d1dd1c9
webNekit <mochalov-hunter@mail.ru>
2024-03-19 17:06:22 +0300

    Установили библиотеку axios

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,9 +6,10 @@ const About = () => {
       <div className="container mx-auto">
         <div className="w-full flex flex-col gap-10">
             <h2 className="h2 text-center">О нашем клубе</h2>
-            <div className="grid grid-cols-2 md:grid-cols-4">
+            <div className="grid gap-4 grid-cols-2 md:grid-cols-4">
                 <div className="w-full h-full border border-zinc-800 p-5 rounded-xl flex flex-col items-center justify-center gap-4">
-
+                    <div className='w-[110px] h-[110px] flex items-center justify-center border border-white/70 text-accent rounded-full text-4xl font-semibold'>200</div>
+                    <h3 className="text-center text-3xl">Игровых зон</h3>
                 </div>
             </div>
         </div>
