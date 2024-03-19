1243cc46cf6377f3fb884791795081aaea36b517
webNekit <mochalov-hunter@mail.ru>
2024-03-19 16:21:53 +0300

    начали делать раздел ABOUT

---
+++ src/components/About/About.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='w-full py-16'>
+      <div className="container mx-auto">
+        <div className="w-full flex flex-col gap-10">
+            <h2 className="h2 text-center">О нашем клубе</h2>
+            <div className="grid grid-cols-2 md:grid-cols-4">
+                <div className="w-full h-full border border-zinc-800 p-5 rounded-xl flex flex-col items-center justify-center gap-4">
+
+                </div>
+            </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About
